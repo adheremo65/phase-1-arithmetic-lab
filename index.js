e82429b1294526b13feb5628119ad164134b3b71
Abdela umer <adheremo65@gmail.com>
2023-03-20 02:13:31 -0400

    arthimetic lab done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+  let random;
+  function ran() {
+    random = parseInt(Math.random()+ 1); 
+  return random;
+}
+ran();
 
-// Write your code here
+let multiply;
+let num1 = 2;
+let num2 =31;
+function mul(){
+multiply = num1*num2;
+return multiply;
+}
+mul();
+let mod;
+let num3 = 14;
+let num4 = 10;
+function rem() {
+    mod = num3%num4;
+    return mod;
+}
+rem();
+
+let max;
+let numa = 4;
+let numb = 5;
+let numc = 20;
+function maxx(){
+    max = Math.max(numa,numb,numc);
+    return max;
+}
+maxx();
+
